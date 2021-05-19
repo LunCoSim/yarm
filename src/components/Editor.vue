51597f7ac5e96ba8ce7ff57f237ef090558cfe02
Nicolay i <nic.ilinov@gmail.com>
2021-05-19 22:28:30 +0700

    добавил значения по  умолчанию Editor

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -13,6 +13,14 @@ export default {
   mounted() {
     this.editor = new EditorJS({
       holder: 'editorjs',
+      data: {
+        blocks: [{
+          "type": "paragraph",
+          "data": {
+            "text": "Hey. Meet the new Editor. On this page you can see it in action — try to edit this text. Source code of the page contains the example of connection and configuration."
+          }
+        }]
+      }
     })
   }
 }
