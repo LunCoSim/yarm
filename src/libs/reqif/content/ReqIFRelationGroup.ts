53282e0154f1db50a5f7dbaec85809d46abc6572
IonRod <rrod76@gmail.com>
2021-05-21 00:03:07 +0300

    - Moved ReqIf to libs/ subfolder

--- src/libs/reqif/content/ReqIFRelationGroup.ts
+++ src/libs/reqif/content/ReqIFRelationGroup.ts
@@ -1,3 +1,6 @@
+import { SpecRelation } from "./ReqIFSpecRelation";
+import { RelationGroupType, SpecElementWithAttributes } from "./ReqIFSpecTypes";
+
 export interface RelationGroup extends SpecElementWithAttributes {
     type: RelationGroupType;
     specRelations: SpecRelation[];
--- src/libs/reqif/content/ReqIFSpecRelation.ts
+++ src/libs/reqif/content/ReqIFSpecRelation.ts
@@ -1,3 +1,6 @@
+import { RelationGroup } from "./ReqIFRelationGroup";
+import { SpecElementWithAttributes, SpecRelationType } from "./ReqIFSpecTypes";
+
 export interface SpecRelation extends SpecElementWithAttributes {
     type: SpecRelationType;
     
