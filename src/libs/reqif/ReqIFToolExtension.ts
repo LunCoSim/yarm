160d9b044f2b01694d7903f97702e381006d574e
IonRod <rrod76@gmail.com>
2021-05-22 14:20:00 +0300

    - Minor update in the extentions

--- src/libs/reqif/ReqIFToolExtension.ts
+++ src/libs/reqif/ReqIFToolExtension.ts
@@ -1,8 +1,8 @@
 //TODO: write correct implementation
 
 export class ReqIFToolExtension {
-    dummydata: string; //temporary data to prevent linter error. To be removed
+    extensions: string[]; //temporary data to prevent linter error. To be removed
     constructor() {
-        this.dummydata = ""
+        this.extensions = []
     }
 }
