16a5b39e7c3cdf2707c6a7a54805a20eb878de88
IonRod <rrod76@gmail.com>
2021-05-27 21:19:15 +0300

    - Demo xml parser code

--- src/libs/test_reqif.ts
+++ src/libs/test_reqif.ts
@@ -5,13 +5,21 @@ pne dir into another
 //node ./build/test_reqif.js 
 */
 
+/*
+To use fast xml parser on local machine
+1. install globally using 
+npm install -g fast-xml-parser
+2. npm link fast-xml-parser
+*/
 
 import { sample_xml } from "./sample_xml";
+import { parse } from "fast-xml-parser";
 
 import { ReqIF } from "./reqif/ReqIF"
 
 const r = new ReqIF();
 
-console.log("Hello");
-console.log(sample_xml);
+const j = parse(sample_xml);
+
+console.log(j["REQ-IF"]['THE-HEADER']);
 
